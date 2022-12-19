612daf25a41cf23b6451ea3114f7eff16566985f
asen <asen.borisov7@yahoo.com>
2022-12-19 09:51:47 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+API_KEY = "NTWOQZUMVJDZ2B6C"
+
+par = {
+    "function" : "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol" : "TSLA",
+    "apikey" : API_KEY,
+
+}
+#"Time Series (Daily)"]["2022-12-16"]
+
+req = requests.get(STOCK_ENDPOINT, params=par)
+req.raise_for_status()
+data = req.json()["Time Series (Daily)"]
+
+data_list = [value for (key, value) in data.items()]
+yesterday = data_list[0]["4. close"]
+before_yesterday = data_list[1]["4. close"]
+
+difference = (abs(float(yesterday) - float(before_yesterday)))
+diff_percent =round((difference / float(yesterday)) *100,2)
+
+print(diff_percent)
+
+if diff_percent > 4:
+    print("get news")
+
+
+#TODO 2. - Get the day before yesterday's closing stock price
+
+#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
+#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
+#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+
+    ## STEP 2: https://newsapi.org/
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number.
+
+#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+#TODO 9. - Send each article as a separate message via Twilio.
+
+
+
+#Optional TODO: Format the message like this:
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
