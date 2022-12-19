f4b4674af62a0eb027a6cb52f94cbeea9e18d718
asen <asen.borisov7@yahoo.com>
2022-12-19 15:51:08 +0200

    app_done

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import requests
+import twilio
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-API_KEY = "NTWOQZUMVJDZ2B6C"
+API_KEY_STOCK = "NTWOQZUMVJDZ2B6C"
+API_KEY_NEWS = "91f48ba89ac24a0ba7dc3edb5fa5ffe5"
+TWILIO_SID = "You Twilio sid "
+TWILIO_AUTH = "Your Twilio Alth Token"
 
 par = {
     "function" : "TIME_SERIES_DAILY_ADJUSTED",
     "symbol" : "TSLA",
-    "apikey" : API_KEY,
+    "apikey" : API_KEY_STOCK,
 
 }
 #"Time Series (Daily)"]["2022-12-16"]
@@ -23,47 +27,29 @@ yesterday = data_list[0]["4. close"]
 before_yesterday = data_list[1]["4. close"]
 
 difference = (abs(float(yesterday) - float(before_yesterday)))
-diff_percent =round((difference / float(yesterday)) *100,2)
+diff_percent = round((difference / float(yesterday)) *100,2)
 
-print(diff_percent)
+
+
 
 if diff_percent > 4:
-    print("get news")
+    news ={
+        "apiKey": API_KEY_NEWS,
+        "qInTitle": STOCK_NAME}
 
+    news_respones = requests.get(NEWS_ENDPOINT, params=news)
+    news_respones.raise_for_status()
+    articles = news_respones.json()["articles"]
+    articles_3 = articles[:3]
 
-#TODO 2. - Get the day before yesterday's closing stock price
+    formated_3 = [f"{STOCK_NAME}: Change:{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in articles_3
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+    client = Clinet(TWILIO_SID, TWILIO_AUTH)
+    for article in articles_3:
+        message = client.message.create(
+            body=article,
+            from_="Twilio number",
+            to= "Your number"
+        )
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio.
-
-
-
-#Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
